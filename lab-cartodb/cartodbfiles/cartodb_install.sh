27a5bd6784b2f457ddb2e05a783cd66812dd417b
Javier Ramirez <frjaraur@gmail.com>
2016-01-24 18:42:14 +0100

    Linking containers... changing configs on cartodb..

--- lab-cartodb/cartodbfiles/cartodb_install.sh
+++
@@ -1,2 +0,0 @@
-su postgres -c "createuser publicuser -h localhost --no-createrole --no-createdb --no-superuser -U postgres"
-su postgres -c "createuser tileuser -h localhost --no-createrole --no-createdb --no-superuser -U postgres"
