fb42560d79986d33dc970a240b7082109df7d339
Javier Ramirez <frjaraur@gmail.com>
2016-01-28 09:38:58 +0100

    Update docker-entrypoint.sh

--- lab-ldap/docker-entrypoint.sh
+++ lab-ldap/docker-entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e
+o
+echo "starting slapd"
+/usr/sbin/slapd -h "ldap:///" -u openldap -g openldap -d 0
+service supervisor start
+echo "Load default ldap domain ... [cartodb.com] "
+ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/backend.ldif
+ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/sssvlv_load.ldif
+ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/sssvlv_config.ldif
+ldapadd -x -D cn=admin,dc=cartodb,dc=com -w changeme -c -f /tmp/objects.ldif
 
-if [ "$1" = "slapd" ]
-then
-	echo "starting slapd"
-	#service supervisor stop
-	service slapd restart
-	ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/backend.ldif
-	ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/sssvlv_load.ldif
-	ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/sssvlv_config.ldif
-	ldapadd -x -D cn=admin,dc=cartodb,dc=com -w changeme -c -f /tmp/objects.ldif
-	
+echo "Remember to change admin initial password 'changeme' ... "
 
-	/usr/sbin/slapd -h "ldap:///" -u openldap -g openldap -d 0
-	service supervisor start
-else
-
-	exec "$@"
-fi	
+echo
+echo "Starting own command ..."
+echo
+exec "$@"
 
